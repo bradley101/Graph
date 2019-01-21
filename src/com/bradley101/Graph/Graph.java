70eace80f523d96df7b000b219aea89d773e1c92
bradley101 <shantanu.banerjee.vt@gmail.com>
2019-01-22 02:52:56 +0530

    setup Graph and Node classes

--- src/com/bradley101/Graph/Graph.java
+++ src/com/bradley101/Graph/Graph.java
@@ -1,8 +1,59 @@
 package com.bradley101.Graph;
 
-public class Graph {
-    public Graph() {
-        // start
-        // no
-    }
+/*
+    @author Shantanu Banerjee
+    email - shantanu.banerjeee@gmail.com
+    date - 22 Jan 2019, 2:26:34 AM IST
+    
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Graph<T> {
+	public static enum WEIGH {
+		WEIGHTED,
+		NON_WEIGHTED
+	}
+	
+	public static enum DIRECTION {
+		DIRECTED,
+		UNDIRECTED
+	}
+	
+	private List<Node> nodeList;
+	
+	public Graph() {
+		nodeList = new ArrayList<>();
+	}
+	
+	public Node<T> newNode() {
+		return new Node<>();
+	}
+	
+	private class Node<U extends T> {
+		private U val;
+		private List<Node> adjacentNodes;
+		
+		private Node() {
+			initializeAdjacentList();
+		}
+		
+		private Node(U val) {
+			this.val = val;
+			initializeAdjacentList();
+		}
+		
+		private void initializeAdjacentList() {
+			adjacentNodes = new ArrayList<>();
+		}
+		
+		public U getVal() {
+			return val;
+		}
+		
+		public void setVal(U val) {
+			this.val = val;
+		}
+	}
 }
