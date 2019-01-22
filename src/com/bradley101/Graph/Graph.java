a03d3aff65e0d41af541580703a41069fd69b189
Shantanu Banerjee <shantanu.banerjee.vt@gmail.com>
2019-01-22 07:52:24 -0500

    remove GraphException inner class
    fixed compile time errors

--- src/com/bradley101/Graph/Graph.java
+++ src/com/bradley101/Graph/Graph.java
@@ -12,131 +12,120 @@ import java.util.List;
 
 public class Graph<T, E> {
 	public static enum WEIGH {
-		WEIGHTED,
-		NON_WEIGHTED
+		WEIGHTED, NON_WEIGHTED
 	}
-	
+
 	public static enum DIRECTION {
-		DIRECTED,
-		UNDIRECTED
+		DIRECTED, UNDIRECTED
 	}
-	
-  private WEIGH weigh; 
-  private DIRECTION dir;
+
+	private WEIGH weigh;
+	private DIRECTION dir;
 	private List<Node> nodeList;
-	
+
 	public Graph() {
-		Graph(WEIGH.WEIGHTED, DIRECTION.UNDIRECTED);
+		this(WEIGH.WEIGHTED, DIRECTION.UNDIRECTED);
 	}
 
-  public Graph(WEIGH weigh, DIRECTION dir) {
-    this.weigh = weigh;
-    this.dir = dir;
-    nodeList = new ArrayList<>();
-  }
+	public Graph(WEIGH weigh, DIRECTION dir) {
+		this.weigh = weigh;
+		this.dir = dir;
+		nodeList = new ArrayList<>();
+	}
 
-  public DIRECTION getDir() {
-    return dir;
-  }
+	public DIRECTION getDir() {
+		return dir;
+	}
 
-  public WEIGH getWeigh() {
-    return weigh;
-  }
-	
-  public Node<T, E> newNode() {
-    return newNode(null);
-  }
+	public WEIGH getWeigh() {
+		return weigh;
+	}
+
+	public Node<T, E> newNode() {
+		return newNode(null);
+	}
 
 	public Node<T, E> newNode(T val) {
 		return new Node<>(val);
 	}
-	
+
 	private class Node<U extends T, F extends E> {
 		private U val;
 		private List<Node> adjacentNodes;
-    private List<Edge> connectingEdges;
-		
+		private List<Edge> connectingEdges;
+
 		private Node() {
-			Node(null);
+			this(null);
 		}
-		
+
 		private Node(U val) {
 			this.val = val;
 			initializeAdjacentList();
 		}
-		
+
 		private void initializeAdjacentList() {
 			adjacentNodes = new ArrayList<>();
-      connectingEdges = new ArrayList<>();
+			connectingEdges = new ArrayList<>();
 		}
 
-    private void addAdjacentNode(Node node) {
-      adjacentNodes.add(node);
-    }
+		private void addAdjacentNode(Node node) {
+			adjacentNodes.add(node);
+		}
 
-    public void connectTo(Node adjacentNode) {
-      connectTo(adjacentNode, null);
-    }
+		public void connectTo(Node adjacentNode) throws GraphException {
+			connectTo(adjacentNode, null);
+		}
 
-    public void connectTo(Node adjacentNode, F data) {
-      if (adjacentNode == null) {
-        throw new GraphException("Cannot add an edge to a null node");
-      }
-      Edge<F> edge = new Edge(data, this, adjacentNode);
-      connectingEdges.add(edge);
-    }
-		
+		public void connectTo(Node adjacentNode, F data) throws GraphException {
+			if (adjacentNode == null) {
+				throw new GraphException("Cannot add an edge to a null node");
+			}
+			Edge<F> edge = new Edge(data, this, adjacentNode);
+			connectingEdges.add(edge);
+		}
+
 		public U getVal() {
 			return val;
 		}
-		
+
 		public void setVal(U val) {
 			this.val = val;
 		}
 	}
 
-  class Edge<E> {
-    E data;
-    Node connectorNode, connectingNode;
+	class Edge<E> {
+		E data;
+		Node connectorNode, connectingNode;
 
-    Edge(Node connectorNode, Node connectingNode) {
-      Edge(null, connectorNode, connectingNode);
-    }
+		Edge(Node connectorNode, Node connectingNode) throws GraphException {
+			this(null, connectorNode, connectingNode);
+		}
 
-    Edge(E data, Node connectorNode, Node connectingNode) {
-      if (connectorNode == null || connectingNode == null) {
-        throw new GraphException("Connecting  nodes cannot be null");
-      }
-      this.data = data;
-      this.connectingNode = connectingNode;
-      this.connectorNode = connectorNode;
+		Edge(E data, Node connectorNode, Node connectingNode) throws GraphException {
+			if (connectorNode == null || connectingNode == null) {
+				throw new GraphException("Connecting  nodes cannot be null");
+			}
+			this.data = data;
+			this.connectingNode = connectingNode;
+			this.connectorNode = connectorNode;
 
-      connectorNode.addAdjacentNode(connectingNode);
-      if (Graph.this.getDir() == Graph.DIRECTION.UNDIRECTED) {
-        connectingNode.addAdjacentNode(connectorNode);
-      } 
-    }
+			connectorNode.addAdjacentNode(connectingNode);
+			if (Graph.this.getDir() == Graph.DIRECTION.UNDIRECTED) {
+				connectingNode.addAdjacentNode(connectorNode);
+			}
+		}
 
-    public Node[] getAdjacentNodes() {
-      return new Node[] {connectorNode, connectingNode};
-    }
+		public Node[] getAdjacentNodes() {
+			return new Node[] { connectorNode, connectingNode };
+		}
 
-    public E getData() {
-      return data;
-    }
+		public E getData() {
+			return data;
+		}
 
-    public void setData(E data) {
-      this.data = data;
-    }
+		public void setData(E data) {
+			this.data = data;
+		}
 
-
-  }
-
-  protected class GraphException extends Exception {
-    private static final long serialVersionUID = 1L;
-
-    GraphException(String msg) {
-      super(msg);
-    }
-  }
+	}
 }
